cf1787da4bfeef2b63ccd6821400c4d22a7cbcc1
Tuấn Vương <vanhtuan0409@gmail.com>
2017-12-22 09:14:23 +0700

    initialize api server

---
+++ apiServer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
